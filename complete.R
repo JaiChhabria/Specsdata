09fa2ed15989c6019339a3adef58c80ace773e6f
JaiChhabria <31588714+JaiChhabria@users.noreply.github.com>
2020-12-09 21:32:01 +0530

    Uploaded

---
+++ complete.R
@@ -0,0 +1,15 @@
+complete <- function(directory, id= 1:332){
+        filelist <- list.files(path = directory, pattern = ".csv", full.names = TRUE)
+        nobs <- numeric()
+        
+        for (i in id){
+                data <- read.csv(filelist[i])
+                nobs <- c(nobs, sum(complete.cases(data)))
+        }
+        
+        data.frame(id, nobs)
+}
+
+complete("/Users/jaic208/Desktop/specdata/",1)
+
+
